4a3b4bd58ef89c18749009e0fcf71aab20640a00
sandeep2194 <sandeep2194@live.com>
2021-05-17 16:07:27 +0530

    staring

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ class BooksApp extends React.Component {
                   However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
                   you don't find a specific author or title. Every search is limited by search terms.
                 */}
-                <input type="text" placeholder="Search by title or author"/>
+                <input type="text" placeholder="Search by title or author" />
 
               </div>
             </div>
